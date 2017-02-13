75a3d694d77a41ee762a1346a50a62229bdd3af0
Stephen Burden <spburden@hotmail.com>
2017-02-12 22:38:45 -0800

    add heroku stuff

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,6 @@
     paths: {
       // paths serve as alias
       'npm:': 'node_modules/'
-       baseURL: '/meal-tracker-angular2/'
     },
     // map tells the System loader where to look for things
     map: {

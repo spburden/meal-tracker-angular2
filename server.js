fbf7ff91cca865f60f2fc06931beb04ae91b974b
Stephen Burden <spburden@hotmail.com>
2017-02-12 23:44:35 -0800

    change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname));
 
 // set the home page route
 app.get('/', function(req, res) {
